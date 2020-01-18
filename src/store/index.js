11cd9308746486149641cd2eb0837bc84997d806
Kristian Larsen <klarsen@netflix.com>
2020-01-18 10:02:44 +0800

    added store placeholder

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    returnedData: [],
+    loading: true,
+  },
+
+  getters: {
+    // Here we will create a getter
+  },
+
+  mutations: {
+    updateData(state, data) {
+      state.returnedData = data
+    },
+    changeLoadingState(state, loading) {
+      state.loading = loading
+    },
+  },
+
+  actions: {
+    async loadData({ commit }) {
+      const data = await getData()
+      commit('updateData', data)
+      commit('changeLoadingState', false)
+    },
+  },
 })
